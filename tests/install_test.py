0ba8fa8df53f9e8c738f0785be2513f13dfdbe22
Antoine Accelio <antoineacc@gmail.com>
2026-05-05 08:37:29 +0200

    Created project structure & added requirements

---
+++ tests/install_test.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import yaml
+
+print("Environment OK")
+print("OpenCV version:", cv2.__version__)
+print("NumPy version:", np.__version__)
+print("Pandas version:", pd.__version__)
